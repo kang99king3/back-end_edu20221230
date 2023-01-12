a72f0f34cd6407d120b6e5747f5bc1d11b6db2dd
user <user@bjkang>
2023-01-12 16:58:31 +0900

    과제추가

--- back_end_javaproject/src/edu_20230112/hk/day8/StringTestMain.java
+++ back_end_javaproject/src/edu_20230112/hk/day8/StringTestMain.java
@@ -8,7 +8,8 @@ public class StringTestMain {
 		System.out.println(smt.sTest2("최강야구"));   
 		smt.sTest4();
 		System.out.println(smt.sTest05("한경닷컴IT교육센터"));
-		
+		System.out.println("=========과제===================");
+		smt.search("이태원");
 	}
 
 }

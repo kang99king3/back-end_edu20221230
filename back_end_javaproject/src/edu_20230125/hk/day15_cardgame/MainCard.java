630de9ce66f06f11716067691327811136f630d8
user <user@bjkang>
2023-01-26 17:01:05 +0900

    MainCard클래스 추가

---
+++ back_end_javaproject/src/edu_20230125/hk/day15_cardgame/MainCard.java
@@ -0,0 +1,12 @@
+package edu_20230125.hk.day15_cardgame;
+
+public class MainCard {
+
+	public static void main(String[] args) {
+		
+		PlayUtil util=new PlayUtil();
+		util.playGame();
+		
+	}
+
+}

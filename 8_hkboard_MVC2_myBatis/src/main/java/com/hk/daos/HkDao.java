5a28ea1607429cd11b49544b44f3a917b7171516
user <user@bjkang>
2023-03-24 13:56:12 +0900

    hkboardMVC2mybatis dao수정중 getboard()

--- 8_hkboard_MVC2_myBatis/src/main/java/com/hk/daos/HkDao.java
+++ 8_hkboard_MVC2_myBatis/src/main/java/com/hk/daos/HkDao.java
@@ -8,11 +8,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.hk.config.SqlMapConfig;
 import com.hk.datasource.DataBase;
 import com.hk.dtos.HkDto;
 
-public class HkDao extends DataBase{
+public class HkDao extends SqlMapConfig{
 
+	private String namespace="com.hk.board.";//namespace+queryid
+	
 	public HkDao() {//생략가능
 		super();//생략가능
 	}
@@ -21,37 +27,20 @@ public class HkDao extends DataBase{
 	//글목록 조회: List<HkDto> --> 글목록 여러개
 	public List<HkDto> getAllList(){
 		List<HkDto> list=new ArrayList<>();
-		Connection conn=null;// DB 연결 객체
-		PreparedStatement psmt=null;// 쿼리 준비,실행 객체
-		ResultSet rs=null;//결과 받는 객체
-		
-		String sql=" select seq, id, title, content, regdate "
-				 + " from hkboard order by regdate desc ";
+		SqlSession sqlSession=null;
 		
 		try {
-			conn=getConnection();
-			System.out.println("2단계:DB연결성공");
-			psmt=conn.prepareStatement(sql);//쿼리를 작성한 상태
-			System.out.println("3단계:쿼리준비성공");
-			rs=psmt.executeQuery();//쿼리를 실행한다.
-			System.out.println("4단계:쿼리실행성공");
-			while(rs.next()) { // next()란 메서드가 결과값이 있는지 행단위로 확인을 한다.
-				//rs[row,row,row...] ---> list[dto,dto,dto...]
-				HkDto dto=new HkDto();//row하나를 담을 객체
-				dto.setSeq(rs.getInt(1));
-				dto.setId(rs.getString(2));
-				dto.setTitle(rs.getString(3));
-				dto.setContent(rs.getString(4));
-				dto.setRegdate(rs.getDate(5));
-				list.add(dto);
-				System.out.println(dto.toString());
-			}
-			System.out.println("5단계:DB결과받기성공");
-		} catch (SQLException e) {
-			System.out.println("JDBC실패:해당클래스는 "+getClass());
+			//1.sqlSessionFactory객체 구해야 됨--> SqlSession객체를 쓸 수 있다
+			SqlSessionFactory sqlSessionFactory=getSqlSessionFactory();
+			//2.sqlSession객체 구하기
+			sqlSession=sqlSessionFactory.openSession(true);//autocommit:true로 설정
+			//3.쿼리 실행하기:selectList()의 반환타입은 List다
+			list=sqlSession.selectList(namespace+"getAllList");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			close(rs, psmt, conn);			
+		}finally {
+			sqlSession.close();
 		}
 		return list;
 	}
@@ -59,38 +48,15 @@ public class HkDao extends DataBase{
 	//글 상세조회 : 쿼리?, 파리미터?, 반환타입 
 	public HkDto getBoard(int seq){
 		HkDto dto=new HkDto();//row하나를 담을 객체
-		Connection conn=null;// DB 연결 객체
-		PreparedStatement psmt=null;// 쿼리 준비,실행 객체
-		ResultSet rs=null;//결과 받는 객체
-		
-		String sql=" select seq, id, title, content, regdate "
-				 + " from hkboard where seq=? ";
-		
+		SqlSession sqlSession=null;
 		try {
-			conn=getConnection();
-			System.out.println("2단계:DB연결성공");
-			psmt=conn.prepareStatement(sql);//쿼리를 작성한 상태
-			psmt.setInt(1, seq);
-			System.out.println("3단계:쿼리준비성공");
-			rs=psmt.executeQuery();//쿼리를 실행한다.
-			System.out.println("4단계:쿼리실행성공");
-			while(rs.next()) { // next()란 메서드가 결과값이 있는지 행단위로 확인을 한다.
-				//rs[row] ---> dto
-				
-				dto.setSeq(rs.getInt(1));
-				dto.setId(rs.getString(2));
-				dto.setTitle(rs.getString(3));
-				dto.setContent(rs.getString(4));
-				dto.setRegdate(rs.getDate(5));
-			
-				System.out.println(dto.toString());
-			}
-			System.out.println("5단계:DB결과받기성공");
-		} catch (SQLException e) {
+			sqlSession=getSqlSessionFactory().openSession(true);
+			dto=sqlSession.selectOne(namespace+"getBoard", seq);
+		} catch (Exception e) {
 			System.out.println("JDBC실패:해당클래스는 "+getClass());
 			e.printStackTrace();
 		} finally {
-			close(rs, psmt, conn);			
+			sqlSession.close();
 		}
 		return dto;
 	}
